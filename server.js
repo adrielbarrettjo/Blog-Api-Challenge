a4aba74dfca4115ddf9b6eae95409c3ccd9fbd7a
adrielbarrettjo <galadriel@barrett-johnson.net>
2017-12-30 15:34:00 -0500

    added test suite

--- server.js
+++ server.js
@@ -8,9 +8,51 @@ const blogPostsRouter = require('./blogPostsRouter');
 app.use(morgan('common'));
 app.use('./blog-posts', blogPostsRouter);
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+
+// add let server, run server, and close server
+let server;
+
+function runServer() {
+	const port = process.env.PORT || 8080;
+	return new Promise((resolve, reject) => {
+		server = app.listen(port, () => {
+			console.log(`Your app is listening on port ${port}`);
+			resolve (server);
+
+
+		}).on('error', err => {
+			reject(err)
+		});
+	});
+}
+
+function closeServer() {
+	return new Promise((resolve, reject) => {
+		console.log('closing server');
+		server.close(err => {
+			if (err) {
+				reject(err);
+				return;
+			}
+			resolve();
+		});
+	});
+}
+
+
+
+
+
+if (require.main === module) {
+	runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
+
+
+// app.listen(process.env.PORT || 8080, () => {
+//   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+// });
 
 // Use Express router and modularize routes to /blog-posts.
 
